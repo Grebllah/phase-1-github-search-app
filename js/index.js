0f4c5e3c0e8665c20a3c57bc8ede421971dc7306
James Hallberg <Grebllah81788@gmail.com>
2023-08-10 11:57:34 -0400

    github search app fix

--- js/index.js
+++ js/index.js
@@ -11,35 +11,33 @@ document.addEventListener("DOMContentLoaded", function(){
             const response = await fetch (`https://api.github.com/search/users?q=${terms}`,{
                 header: {Accept: "application/vnd.github.v3+json"}
             });
-        return data = await response.json();
+        return await response.json();
         }
 
-        userSearch(searchTerm)
-        .then((data) => {
-            for (user of data.items) {
-                const userName = user.login
-                const userNameLi = document.createElement("li")
-                userList.appendChild(userNameLi).innerText = userName
-                userNameLi.addEventListener("click", function(e) {
-                    async function userRepoSearch(URL2 = "", data2 = {}) {
-                        terms2 = e.target
-                        const response2 = await fetch (`https://api.github.com/users/${terms}/repos`,{
-                            header: {Accept: "application/vnd.github.v3+json"}
-                        });
-                    return data2 = await response2.json();
-                    }
-                
-                userRepoSearch(userName)
-                .then((data) => {
-                    for (repo of data) {
-                    console.log(repo.name)
-                    const repoName = repo.name
-                    const repoLi = document.createElement("li")
-                    repoList.appendChild(repoLi).innerText = repoName
-                    }
-                })
-                })
-            }
-        })
-    });
-})
+        const data = userSearch(searchTerm)
+        for (user of data.items) {
+            const userName = user.login
+            const userNameLi = document.createElement("li")
+            userList.appendChild(userNameLi).innerText = userName
+            userNameLi.addEventListener("click", function(e) {
+                async function userRepoSearch(URL2 = "", data2 = {}) {
+                    terms2 = e.target
+                    const response2 = await fetch (`https://api.github.com/users/${terms}/repos`,{
+                        header: {Accept: "application/vnd.github.v3+json"}
+                    });
+                return data2 = await response2.json();
+                }
+            
+            userRepoSearch(userName)
+            .then((data) => {
+                for (repo of data) {
+                console.log(repo.name)
+                const repoName = repo.name
+                const repoLi = document.createElement("li")
+                repoList.appendChild(repoLi).innerText = repoName
+                }
+            })
+            })
+        }
+    })
+});
